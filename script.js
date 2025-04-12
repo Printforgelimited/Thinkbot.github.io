f5d6f44c17a136bc18f70928f7d8faaddae727b3
Printforgelimited <gncarlton26@icloud.com>
2025-04-11 21:42:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -54,22 +54,6 @@ userInput.addEventListener('keypress', (e) => {
     sendButton.click();
   }
 });
-async function getBotResponse(userMessage) {
-  const response = await fetch("https://api.openai.com/v1/chat/completions", {
-    method: "POST",
-    headers: {
-      "Authorization": "Bearer keyhere",  
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: userMessage }],
-      max_tokens: 150
-    })
-  });
 
-  const data = await response.json();
-  return data.choices[0].message.content;
-}
 
 
